a69c3ff3c7319a6e73ab98103d99d6edb9e89d6a
Creccles <creccles12@gmail.com>
2022-03-30 15:44:31 +0100

    Chapter 6 w/ Exercises

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
-    #  Constuct a dictionary to pass to the template engine as its context.
-    #  Note the key boldmessgae matches to {{  boldmessage  }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {}
 
-    #  Return a rendered response to send to the client
-    #  We make use of the shortcut function to make out lives easier.
-    #  Note that the first parameter is the template we wish to use.
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
@@ -18,3 +19,17 @@ def about(request):
 def second(request):
     context_dict = {'boldmessage': 'Apple, Elephant, Pear, Dog, Orange!'}
     return render(request, 'rango/second.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
--- venv/Scripts/django-admin.py
+++ venv/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\crecc\Downloads\chap5\TangoWithDjango-c01330d87059345440bde544f5fa5e4798826897\venv\Scripts\python.exe
+#!C:\Users\crecc\Downloads\chap6\TangoWithDjango-ecf3b3904720e9564f735b36e5ab14832dcf23a5\venv\Scripts\python.exe
 from django.core import management
 
 if __name__ == "__main__":
