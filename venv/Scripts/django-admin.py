954886ea522719e6fe2f5afb2266a4c79e70a8ef
Creccles <creccles12@gmail.com>
2022-03-30 15:30:26 +0100

    Chapter 3 w/ Exercises

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\crecc\downloads\chap3\tangowithdjango-28e1f45f93fc8806c2799221100d4d1aa42e1541\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
